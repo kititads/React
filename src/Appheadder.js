ecc7a825a3b0700cef312b2f99f2a4f2a68b0afb
kititad <kititads1111@gmail.com>
2021-02-17 12:39:21 +0700

    Workshop

---
+++ src/Appheadder.js
@@ -0,0 +1,9 @@
+function Appheadder(){
+    return (
+          <header className="App-header">
+              
+    
+          </header>
+          );
+        }
+    export default Appheadder ;
---
+++ src/items.js
@@ -0,0 +1,27 @@
+const items =[
+    {
+      title :"Ring Of Kingdom",
+      thumbnaiUrl :"/pic/Pic1.jpg"
+      
+    
+    },
+    {
+      title :"Gura",
+      thumbnaiUrl :"/pic/Pic2.png"
+      
+    
+    },
+    {
+      title :"Korone",
+      thumbnaiUrl :"/pic/Pic3.png"
+      
+    
+    },
+    {
+      title :"Pekora",
+      thumbnaiUrl :"/pic/Pic4.jpg"
+      
+    
+    }
+  ];
+  export default items;
